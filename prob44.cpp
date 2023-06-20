e27cd8421f4ee1e79b172ee708c43355ac022f5a
Vishal <vishalsingh2508720@gmail.com>
2023-06-20 09:56:38 +0530

    44

--- prob44.cpp
+++ prob44.cpp
@@ -1,28 +1,17 @@
-#include<bits/stdc++.h>
-struct meetings{
-int start,end,pos;
-};
-bool comparator(struct meetings m1,struct meetings m2){
-if(m1.end<m2.end)return true;
-else if(m1.end>m2.end)return false;
-else if(m1.pos<m2.pos)return true;
-else return false;
+int calculateMinPatforms(int at[],int dt[],int n){
+sort(at,at+n);
+sort(dt,dt+n);
+int p=1,ans=1;
+int a=1,d=0;
+while(a<n){
+if(at[a]<=dt[d]){
+p++;
+ans=max(ans,p);
+a++;
+}else{
+d++;
+p=p-1;
 }
-vector<int>maximumMeetings(vector<int>&start,vector<int>&end){
-int n=start.size();
-struct meetings meet[n];
-for(int i=0;i<n;i++){
-meet[i].start=start[i],meet[i].end=end[i],meet[i].pos=i+1;
-}
-sort(meet,meet+n,comparator);
-vector<int>ans;
-int limit=meet[0].end;
-ans.push_back(meet[0].pos);
-for(int i=1;i<n;i++){
-if(meet[i].start>limit){
-ans.push_back(meet[i].pos);
-limit=meet[i].end;
-}
-}
+}//end while
 return ans;
 }
