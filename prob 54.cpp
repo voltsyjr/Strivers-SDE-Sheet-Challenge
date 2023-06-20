423095bf451bf60d8f4dc9de0cad1987daa6ca31
Vishal <vishalsingh2508720@gmail.com>
2023-06-21 04:13:02 +0530

    54

---
+++ prob 54.cpp
@@ -0,0 +1,19 @@
+string kthPermutation(int n, int k) {
+    long long fact=1;
+    vector<int>num;
+    for(int i=1;i<n;i++){
+        fact*=i;
+        num.push_back(i);
+    }
+    num.push_back(n);
+    k--;
+    string ans="";
+    while(true){
+        ans+=to_string(num[k/fact]);
+        num.erase(num.begin()+k/fact);
+        if(num.size()==0)break;
+        k=k%fact;
+        fact=fact/num.size();
+    }
+    return ans;
+}
